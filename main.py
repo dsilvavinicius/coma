Bug fixes: a loop in the similarity computation was wrong. Similarity computation is now parallel. In progress: PCA before RBF.

main.py
@@ -11,131 +11,132 @@ import argparse
 from facemesh import FaceData
 from opendr.topology import get_vert_connectivity
 import time
+import multiprocessing
 
-parser = argparse.ArgumentParser(description='Tensorflow Trainer for Convolutional Mesh Autoencoders')
-parser.add_argument('--name', default='bareteeth', help='facial_motion| lfw ')
-parser.add_argument('--data', default='data/bareteeth', help='facial_motion| lfw ')
-parser.add_argument('--batch_size', type=int, default=16, help='input batch size for training (default: 64)')
-parser.add_argument('--num_epochs', type=int, default=200, help='number of epochs to train (default: 2)')
-parser.add_argument('--eval_frequency', type=int, default=200, help='eval frequency')
-parser.add_argument('--filter', default='chebyshev5', help='filter')
-parser.add_argument('--nz', type=int, default=8, help='Size of latent variable')
-parser.add_argument('--lr', type=float, default=8e-3, help='Learning Rate')
-parser.add_argument('--workers', type=int, default=4, help='number of data loading threads')
-parser.add_argument('--no-cuda', action='store_true', default=False, help='enables CUDA training')
-parser.add_argument('--seed', type=int, default=2, help='random seed (default: 1)')
-parser.add_argument('--mode', default='train', type=str, help='train or test')
-parser.add_argument('--viz', type=int, default=0, help='visualize while test')
-parser.add_argument('--loss', default='l1', help='l1 or l2')
-parser.add_argument('--mesh1', default='m1', help='for mesh interpolation')
-parser.add_argument('--mesh2', default='m1', help='for mesh interpolation')
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Tensorflow Trainer for Convolutional Mesh Autoencoders')
+    parser.add_argument('--name', default='bareteeth', help='facial_motion| lfw ')
+    parser.add_argument('--data', default='data/bareteeth', help='facial_motion| lfw ')
+    parser.add_argument('--batch_size', type=int, default=16, help='input batch size for training (default: 64)')
+    parser.add_argument('--num_epochs', type=int, default=200, help='number of epochs to train (default: 2)')
+    parser.add_argument('--eval_frequency', type=int, default=200, help='eval frequency')
+    parser.add_argument('--filter', default='chebyshev5', help='filter')
+    parser.add_argument('--nz', type=int, default=8, help='Size of latent variable')
+    parser.add_argument('--lr', type=float, default=8e-3, help='Learning Rate')
+    parser.add_argument('--workers', type=int, default=4, help='number of data loading threads')
+    parser.add_argument('--no-cuda', action='store_true', default=False, help='enables CUDA training')
+    parser.add_argument('--seed', type=int, default=2, help='random seed (default: 1)')
+    parser.add_argument('--mode', default='train', type=str, help='train or test')
+    parser.add_argument('--viz', type=int, default=0, help='visualize while test')
+    parser.add_argument('--loss', default='l1', help='l1 or l2')
+    parser.add_argument('--mesh1', default='m1', help='for mesh interpolation')
+    parser.add_argument('--mesh2', default='m1', help='for mesh interpolation')
+
+    args = parser.parse_args()
+
+    np.random.seed(args.seed)
+    nz = args.nz
+    print("Loading data .. ")
+    reference_mesh_file = 'data/template.obj'
+    facedata = FaceData(nVal=100, train_file=args.data+'/train.npy',
+        test_file=args.data+'/test.npy', reference_mesh_file=reference_mesh_file, pca_n_comp=nz)
+
+    ds_factors = [4,4,4,4]	# Sampling factor of the mesh at each stage of sampling
+    print("Generating Transform Matrices ..")
+
+    # Generates adjecency matrices A, downsampling matrices D, and upsamling matrices U by sampling
+    # the mesh 4 times. Each time the mesh is sampled by a factor of 4
+
+    M,A,D,U = mesh_sampling.generate_transform_matrices(facedata.reference_mesh, ds_factors)
+
+    A = list(map(lambda x:x.astype('float32'), A))
+    D = list(map(lambda x:x.astype('float32'), D))
+    U = list(map(lambda x:x.astype('float32'), U))
+    p = list(map(lambda x:x.shape[0], A))
+
+    X_train = facedata.vertices_train.astype('float32')
+    X_val = facedata.vertices_val.astype('float32')
+    X_test = facedata.vertices_test.astype('float32')
+
+    print("Computing Graph Laplacians ..")
+    L = [graph.laplacian(a, normalized=True) for a in A]
+
+    n_train = X_train.shape[0]
+    params = dict()
+    params['dir_name']       = args.name
+    params['num_epochs']     = args.num_epochs
+    params['batch_size']     = args.batch_size
+    params['eval_frequency'] = args.eval_frequency
+    # Building blocks.
+    params['filter']         = args.filter
+    params['brelu']          = 'b1relu'
+    params['pool']           = 'poolwT'
+    params['unpool']		 = 'poolwT'
 
 
-args = parser.parse_args()
+    # Architecture.
+    params['F_0']            = int(X_train.shape[2])  # Number of graph input features.
+    params['F']              = [16, 16, 16, 32]  # Number of graph convolutional filters.
+    params['K']              = [6, 6, 6, 6]  # Polynomial orders.
+    params['p']              = p #[4, 4, 4, 4]    # Pooling sizes.
+    params['nz']              = [nz]  # Output dimensionality of fully connected layers.
 
-np.random.seed(args.seed)
-nz = args.nz
-print("Loading data .. ")
-reference_mesh_file = 'data/template.obj'
-facedata = FaceData(nVal=100, train_file=args.data+'/train.npy',
-    test_file=args.data+'/test.npy', reference_mesh_file=reference_mesh_file, pca_n_comp=nz)
+    # Optimization.
+    params['which_loss']     = args.loss
+    params['nv']             = facedata.n_vertex
+    params['regularization'] = 5e-4
+    params['dropout']        = 1
+    params['learning_rate']  = args.lr
+    params['decay_rate']     = 0.99
+    params['momentum']       = 0.9
+    params['decay_steps']    = n_train / params['batch_size']
 
-ds_factors = [4,4,4,4]	# Sampling factor of the mesh at each stage of sampling
-print("Generating Transform Matrices ..")
+    model = models.coma(L=L, D=D, U=U, **params)
 
-# Generates adjecency matrices A, downsampling matrices D, and upsamling matrices U by sampling
-# the mesh 4 times. Each time the mesh is sampled by a factor of 4
-
-M,A,D,U = mesh_sampling.generate_transform_matrices(facedata.reference_mesh, ds_factors)
-
-A = list(map(lambda x:x.astype('float32'), A))
-D = list(map(lambda x:x.astype('float32'), D))
-U = list(map(lambda x:x.astype('float32'), U))
-p = list(map(lambda x:x.shape[0], A))
-
-X_train = facedata.vertices_train.astype('float32')
-X_val = facedata.vertices_val.astype('float32')
-X_test = facedata.vertices_test.astype('float32')
-
-print("Computing Graph Laplacians ..")
-L = [graph.laplacian(a, normalized=True) for a in A]
-
-n_train = X_train.shape[0]
-params = dict()
-params['dir_name']       = args.name
-params['num_epochs']     = args.num_epochs
-params['batch_size']     = args.batch_size
-params['eval_frequency'] = args.eval_frequency
-# Building blocks.
-params['filter']         = args.filter
-params['brelu']          = 'b1relu'
-params['pool']           = 'poolwT'
-params['unpool']		 = 'poolwT'
-
-
-# Architecture.
-params['F_0']            = int(X_train.shape[2])  # Number of graph input features.
-params['F']              = [16, 16, 16, 32]  # Number of graph convolutional filters.
-params['K']              = [6, 6, 6, 6]  # Polynomial orders.
-params['p']              = p #[4, 4, 4, 4]    # Pooling sizes.
-params['nz']              = [nz]  # Output dimensionality of fully connected layers.
-
-# Optimization.
-params['which_loss']     = args.loss
-params['nv']             = facedata.n_vertex
-params['regularization'] = 5e-4
-params['dropout']        = 1
-params['learning_rate']  = args.lr
-params['decay_rate']     = 0.99
-params['momentum']       = 0.9
-params['decay_steps']    = n_train / params['batch_size']
-
-model = models.coma(L=L, D=D, U=U, **params)
-
-if args.mode in ['test']:
-    if not os.path.exists('results'):
-        os.makedirs('results')
-    predictions, loss = model.predict(X_test, X_test)
-    print("L1 Loss= ", loss)
-    euclidean_loss = np.mean(np.sqrt(np.sum((facedata.std*(predictions-facedata.vertices_test))**2, axis=2)))
-    print("Euclidean loss= ", euclidean_loss)
-    np.save('results/'+args.name+'_predictions', predictions)
-    if args.viz:
-        from psbody.mesh import MeshViewers
-        viewer_recon = MeshViewers(window_width=800, window_height=1000, shape=[5, 4], titlebar='Mesh Reconstructions')
-        for i in range(predictions.shape[0]//20):
-            facedata.show_mesh(viewer=viewer_recon, mesh_vecs=predictions[i*20:(i+1)*20], figsize=(5,4))
-            time.sleep(0.1)
-elif args.mode in ['sample']:
-    meshes = facedata.get_normalized_meshes(args.mesh1, args.mesh2)
-    features = model.encode(meshes)
-elif args.mode in ['latent']:
-    LatentSpaceVisualization(model, facedata)
-elif args.mode in ['project']:
-    proj_types = ('coma', 'mds', 'tsne')
-    projections = [Projection(proj_type, args.name, facedata, model) for proj_type in proj_types]
-    inverses = ['coma', 'lamp', 'lamp']
-    mesh_visualizer = LatentSpaceVisualization(model, facedata, viewer_size=(1080, 1080))
-    ProjectionUI(args.name, projections, inverses, mesh_visualizer, fig_size=(14.0, 10.0), fig_pos=(1080, 0))
-elif args.mode in ['project_load']:
-    proj_types = ('coma', 'mds', 'tsne')
-    projections = [Projection(proj_type, args.name, facedata) for proj_type in proj_types]
-    projections.append(projections[1])
-    inverses = ['coma', 'lamp', 'lamp', 'rbf']
-    mesh_visualizer = LatentSpaceVisualization(model, facedata, viewer_size=(1080, 1080))
-    ProjectionUI(args.name, projections, inverses, mesh_visualizer, fig_size=(14.0, 10.0), fig_pos=(1080, 0))
-elif args.mode in ['project_test']:
-    # Minimize dataset size.
-    original_shape = facedata.vertices_test.shape
-    facedata.vertices_test.resize(6, original_shape[1], original_shape[2])
-    projection = Projection('mds', args.name + '_test', facedata, model)
-    mesh_visualizer = LatentSpaceVisualization(model, facedata, viewer_size=(1080, 1080))
-    ProjectionUI(args.name, [projection], ['rbf'], mesh_visualizer, fig_size=(14.0, 10.0), fig_pos=(1080, 0))
-else:
-    if not os.path.exists(os.path.join('checkpoints', args.name)):
-        os.makedirs(os.path.join('checkpoints', args.name))
-    with open(os.path.join('checkpoints', args.name +'params.json'),'w') as fp:
-        saveparams = copy.deepcopy(params)
-        saveparams['seed'] = args.seed
-        json.dump(saveparams, fp)
-    loss, t_step = model.fit(X_train, X_train, X_val, X_val)
+    if args.mode in ['test']:
+        if not os.path.exists('results'):
+            os.makedirs('results')
+        predictions, loss = model.predict(X_test, X_test)
+        print("L1 Loss= ", loss)
+        euclidean_loss = np.mean(np.sqrt(np.sum((facedata.std*(predictions-facedata.vertices_test))**2, axis=2)))
+        print("Euclidean loss= ", euclidean_loss)
+        np.save('results/'+args.name+'_predictions', predictions)
+        if args.viz:
+            from psbody.mesh import MeshViewers
+            viewer_recon = MeshViewers(window_width=800, window_height=1000, shape=[5, 4], titlebar='Mesh Reconstructions')
+            for i in range(predictions.shape[0]//20):
+                facedata.show_mesh(viewer=viewer_recon, mesh_vecs=predictions[i*20:(i+1)*20], figsize=(5,4))
+                time.sleep(0.1)
+    elif args.mode in ['sample']:
+        meshes = facedata.get_normalized_meshes(args.mesh1, args.mesh2)
+        features = model.encode(meshes)
+    elif args.mode in ['latent']:
+        LatentSpaceVisualization(model, facedata)
+    elif args.mode in ['project']:
+        proj_types = ('coma', 'mds', 'tsne', 'pca_mds')
+        projections = [Projection(proj_type, args.name, facedata, model) for proj_type in proj_types]
+        inverses = ['coma', 'lamp', 'lamp', 'rbf']
+        mesh_visualizer = LatentSpaceVisualization(model, facedata, viewer_size=(1080, 1080))
+        ProjectionUI(args.name, projections, inverses, mesh_visualizer, fig_size=(14.0, 10.0), fig_pos=(1080, 0))
+    elif args.mode in ['project_load']:
+        proj_types = ('coma', 'mds', 'tsne')
+        projections = [Projection(proj_type, args.name, facedata) for proj_type in proj_types]
+        projections.append(projections[1])
+        inverses = ['coma', 'lamp', 'lamp', 'rbf']
+        mesh_visualizer = LatentSpaceVisualization(model, facedata, viewer_size=(1080, 1080))
+        ProjectionUI(args.name, projections, inverses, mesh_visualizer, fig_size=(14.0, 10.0), fig_pos=(1080, 0))
+    elif args.mode in ['project_test']:
+        # Minimize dataset size.
+        original_shape = facedata.vertices_test.shape
+        facedata.vertices_test.resize(500, original_shape[1], original_shape[2])
+        projection = Projection('pca_mds', args.name + '_test', facedata, model)
+        mesh_visualizer = LatentSpaceVisualization(model, facedata, viewer_size=(1080, 1080))
+        ProjectionUI(args.name, [projection], ['rbf'], mesh_visualizer, fig_size=(14.0, 10.0), fig_pos=(1080, 0))
+    else:
+        if not os.path.exists(os.path.join('checkpoints', args.name)):
+            os.makedirs(os.path.join('checkpoints', args.name))
+        with open(os.path.join('checkpoints', args.name +'params.json'),'w') as fp:
+            saveparams = copy.deepcopy(params)
+            saveparams['seed'] = args.seed
+            json.dump(saveparams, fp)
+        loss, t_step = model.fit(X_train, X_train, X_val, X_val)

projections/projection.py
@@ -1,5 +1,7 @@
 import numpy as np
+from multiprocessing import Pool
 from sklearn.manifold import MDS, TSNE
+from projections.pca_projection import PCAProjection
 from os import path, makedirs
 
 
@@ -8,7 +10,7 @@ def get_files(proj_type, data_name):
     main_dir = path.join('results', 'projections', data_name)
     proj_dir = path.join(main_dir, proj_type)
     makedirs(proj_dir, exist_ok=True)
-    files = {'meshes_similarity': path.join(main_dir, 'meshes_similarity.npy'),
+    files = {'meshes_similarity': path.join(proj_dir, 'meshes_similarity.npy'),
              'projections': path.join(proj_dir, 'projection.npy'),
              'projections_similarity': path.join(proj_dir, 'projections_similarity.npy'),
              'local_stress': path.join(proj_dir, 'local_stress.npy'),
@@ -16,36 +18,39 @@ def get_files(proj_type, data_name):
     return files
 
 
+def compute_vertex_distance(vertices, i, j, k):
+    """Computes the distance of the same vertex k in meshes i and j."""
+    vertex_dim = vertices.shape[-1]
+    # l is used to loop the vertex coordinates.
+    distance_vector = [vertices[i][k][l] - vertices[j][k][l]
+                       for l in range(0, vertex_dim)]
+    distance_vector = np.array(distance_vector)
+    return np.linalg.norm(distance_vector)
+
+
+def compute_mesh_distances(vertices, i, j):
+    """Computes the distance between meshes i and j."""
+    if i < j:
+        vertices_dist = [compute_vertex_distance(vertices, i, j, k)
+                         for k in range(0, vertices.shape[1])]
+        vertices_dist = np.array(vertices_dist)
+        return vertices_dist.mean()
+    return 0.0
+
+
+def compute_projection_distances(projections, i, j):
+    """Computes the distance between projection of meshes i and j."""
+    if i < j:
+        return compute_vertex_distance(projections, i, j, 0)
+    return 0.0
+
+
 class Projection:
     """Tools for projecting meshes (in the dimensionality reduction sense) to a plane and comparing the projections."""
-
-    def __compute_vertex_distance(self, i, j, k):
-        """Computes the distance of the same vertex k in meshes i and j."""
-        vertex_dim = self.vertices.shape[2]
-        # l is used to loop the vertex coordinates.
-        distance_vector = [self.vertices[i][k][l] - self.vertices[j][k][l]
-                           for l in range(0, vertex_dim)]
-        distance_vector = np.array(distance_vector)
-        return np.linalg.norm(distance_vector)
-
-    def __compute_mesh_distances(self, i, j):
-        """Computes the distance between meshes i and j."""
-        if i < j:
-            vertices_dist = [self.__compute_vertex_distance(i, j, k)
-                             for k in range(0, self.vertices.shape[2])]
-            vertices_dist = np.array(vertices_dist)
-            return vertices_dist.mean()
-        return 0.0
 
     def __project_coma(self, i):
         """Projects mesh i."""
         return self.model.encode(np.array([self.vertices[i]]))
-
-    def __compute_projection_distances(self, i, j):
-        """Computes the distance between projection of meshes i and j."""
-        if i < j:
-            return self.__compute_vertex_distance(i, j, 0)
-        return 0.0
 
     # Second implementation
     # def compute_stress(self):
@@ -82,7 +87,12 @@ class Projection:
     def __init__(self, proj_type, data_name, facedata, model=None):
         self.proj_type = proj_type
         print('Projection type: ' + proj_type)
-        self.vertices = facedata.vertices_test
+
+        if proj_type == 'pca_mds':
+            self.pca_proj = PCAProjection(facedata.vertices_test, 2)
+            self.vertices = self.pca_proj.proj_X
+        else:
+            self.vertices = facedata.vertices_test
 
         files = get_files(proj_type, data_name)
 
@@ -109,23 +119,21 @@ class Projection:
             print('Facedata vertex matrix shape: ' + str(shape))
 
             print('Computing mesh distances...')
-            self.meshes_similarity = np.array([self.__compute_mesh_distances(i, j)
-                                               for i in range(0, shape[0])
-                                               for j in range(0, shape[0])])
+            pool = Pool()
+            params = [(self.vertices, i, j)
+                      for i in range(0, shape[0])
+                      for j in range(0, shape[0])]
+            self.meshes_similarity = np.array(pool.starmap(compute_mesh_distances, params))
             self.meshes_similarity.shape = (shape[0], shape[0])
 
-            # DEBUG
-            # self.meshes_similarity = np.load(files['meshes_similarity'])
-            #
-
-            print('Similarity meshes: ' + str(self.meshes_similarity))
+            print('Similarity meshes: ' + str(self.meshes_similarity.shape))
             np.save(files['meshes_similarity'], self.meshes_similarity)
 
             print('Projecting...')
             if proj_type == 'coma':
                 self.projections = np.array([self.__project_coma(i)
                                             for i in range(0, shape[0])])
-            elif proj_type == 'mds':
+            elif proj_type in ['mds', 'pca_mds']:
                 embedding = MDS(dissimilarity='precomputed')
                 meshes_similarity = self.meshes_similarity + self.meshes_similarity.transpose()
                 self.projections = np.array(embedding.fit_transform(meshes_similarity))
@@ -138,16 +146,21 @@ class Projection:
             else:
                 raise ValueError('Unexpected projection type.')
 
-            self.projections.shape = (shape[0], 2)
+            # Reshape to reuse similarity computation for vertices.
+            self.projections.shape = (shape[0], 1, 2)
 
-            print('Projections: ' + str(self.projections))
+            print('Projections: ' + str(self.projections.shape))
             np.save(files['projections'], self.projections)
 
             print('Computing projection distances...')
-            self.projections_similarity = np.array([self.__compute_projection_distances(i, j)
-                                                    for i in range(0, shape[0])
-                                                    for j in range(0, shape[0])])
+            params = [(self.projections, i, j)
+                      for i in range(0, shape[0])
+                      for j in range(0, shape[0])]
+            self.projections_similarity = np.array(pool.starmap(compute_projection_distances, params))
             self.projections_similarity.shape = (shape[0], shape[0])
+
+            # Reshape to drop additional 1-size dimension.
+            self.projections.shape = (shape[0], 2)
 
             # TEST
             # self.projections_similarity = np.zeros((2, 2))

projections/rbf.py
@@ -14,7 +14,7 @@ class Rbf:
             # multiquadrics
             return np.sqrt(self.c * self.c + self.e * r * r)
 
-    def __init__(self, x, y, c, e, _type='multiquadrics'):
+    def __init__(self, x, y, pca, c, e, _type='multiquadrics'):
         """ Constructor.
         Parameters
         ----------
@@ -24,6 +24,8 @@ class Rbf:
         y : array
             Array with the projections of the meshes in `x`. Its shape must be (N, 2), where N is the number of meshes.
             Last dimension is the projection dimension (2 since we are in the plane).
+        pca: PCAProjection
+            PCAProjection used to invert further to mesh space.
         c : float
             first constant for rbf function. Used for multiquadrics.
         e : float
@@ -31,6 +33,7 @@ class Rbf:
         """
         print('Starting RBF inverse projection.')
 
+        self.pca = pca
         self.c = c
         self.e = e
         self.type = _type
@@ -84,5 +87,7 @@ class Rbf:
                      for i in range(0, self.N)]
                 ))
 
-        print('q shape' + str(q.shape))
+        print('q shape before pca: ' + str(q.shape))
+        q = self.pca.invert(q)
+        print('q shape after pca: ' + str(q.shape))
         return q